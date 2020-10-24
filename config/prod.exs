579521a884421dca11a17a0f1f7c0030f3f25cbd
Alejandro Londono <alejandrolondono@tamu.edu>
2020-10-24 17:09:41 -0500

    -

--- config/prod.exs
+++ config/prod.exs
@@ -2,4 +2,3 @@ import Config
 
 config :the_dragon_of_the_rest, TheDragonOfTheRest.Endpoint,
   port: String.to_integer(System.get_env("PORT") || "4000")
-  url: [scheme: "http", host: "the-dragon-of-the-rest.herokuapp.com", port: :port]
