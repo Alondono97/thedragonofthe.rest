0dbbbf4479020926a95cde612404ddc949e025ad
Alejandro Londono <alf0930@gmail.com>
2022-02-06 15:03:44 -0600

    made lotus spin while loading

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 getQuote();
 
 function getQuote() {
+    document.getElementById("tile").className = "loading";
     fetch('https://api.thedragonofthe.rest/quote')
         .then((resp) => resp.json())
         .then(function (data) {
             console.log("data:", data);
             document.getElementById("quote").innerHTML = data.quote;
+            document.getElementById("tile").className = "static";
         });
 }
